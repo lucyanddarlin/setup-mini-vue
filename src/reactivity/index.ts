9397fc9cf542b7b68beb599793fdd9cb89db3a8a
露西 <lucy@luxideMacBook-Air.local>
2023-03-25 21:24:59 +0800

    feat: 实现 proxyRefs

--- src/reactivity/index.ts
+++ src/reactivity/index.ts
@@ -1,3 +1,3 @@
-export const add = (a, b) => {
+export const add = (a: object, b) => {
   return a + b
 }
--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -46,3 +46,18 @@ export function isRef(ref) {
 export function unRef(ref) {
   return isRef(ref) ? ref.value : ref
 }
+
+export function proxyRefs(objectWithRef) {
+  return new Proxy(objectWithRef, {
+    get(target, key) {
+      return unRef(Reflect.get(target, key))
+    },
+    set(target, key, value) {
+      if (isRef(target[key]) && !isRef(value)) {
+        return target[key].value = value
+      } else {
+        return Reflect.set(target, key, value)
+      }
+    }
+  })
+}
