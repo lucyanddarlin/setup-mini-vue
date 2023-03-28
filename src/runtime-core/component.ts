b3fd54b44b969c82653d4c1dceb91806063275ff
露西 <lucy@luxideMacBook-Air.local>
2023-03-28 18:24:18 +0800

    fix: 修复错误

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -22,6 +22,7 @@ function setupStatefulComponent(instance: any) {
     const setupResult = setup()
     handleSetupResult(instance, setupResult)
   }
+  finishComponentSetup(instance)
 }
 
 function handleSetupResult(instance, setupResult: any) {
@@ -29,7 +30,6 @@ function handleSetupResult(instance, setupResult: any) {
   if (typeof setupResult === 'object') {
     instance.setupState = setupResult
   }
-  finishComponentSetup(instance)
 }
 
 function finishComponentSetup(instance: any) {
--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -9,8 +9,6 @@ function patch(vnode, container) {
   processComponent(vnode, container)
 }
 
-
-
 function processComponent(vnode, container) {
   mountComponent(vnode, container)
 }
